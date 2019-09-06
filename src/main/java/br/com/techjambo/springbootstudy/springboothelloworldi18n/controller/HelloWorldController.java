d0c4ab099d22a809f17f4f4e68ab1ff4c9f948e7
edilson.costa <edilson.costa@dataprev.gov.br>
2019-09-06 09:21:54 -0300

    Initial commit

--- src/main/java/br/com/techjambo/springbootstudy/springboothelloworldi18n/controller/HelloWorldController.java
+++ src/main/java/br/com/techjambo/springbootstudy/springboothelloworldi18n/controller/HelloWorldController.java
@@ -1,12 +1,10 @@
 package br.com.techjambo.springbootstudy.springboothelloworldi18n.controller;
 
-import java.util.Locale;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.techjambo.springbootstudy.springboothelloworldi18n.bean.HelloWorldBean;
@@ -34,8 +32,8 @@ public class HelloWorldController {
 	}
 	
 	@GetMapping(path="/hello-world-i18n")
-	public String HelloWorldI18N(@RequestHeader(name="Accept-Language", required=false) Locale locale) {
-		return messageSource.getMessage("hello.world",null, locale);
+	public String HelloWorldI18N() {
+		return messageSource.getMessage("hello.world",null, LocaleContextHolder.getLocale());
 		
 	}
 	
